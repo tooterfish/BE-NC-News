7058aa2c17bf467c4460e5d80247cacce8fd374b
Harry Fleming <email@harryfleming.net>
2023-03-27 16:12:18 +0100

    initial declarations of get/fetchArticles

---
+++ controllers/articles-controllers.js
@@ -0,0 +1,15 @@
+const { fetchArticle, fetchArticles } = require('../models/articles-models')
+
+exports.getArticle = (req, res, next) => {
+  const { article_id } = req.params
+  fetchArticle(article_id)
+  .then((article) => {
+    res.status(200).send({ article })
+  })
+  .catch((err) => {
+    next(err)
+  })
+}
+
+exports.getArticles = (req, res, next) => {
+}
---
+++ models/articles-models.js
@@ -0,0 +1,13 @@
+const db = require('../db/connection')
+
+exports.fetchArticle = (articleId) => {
+  return db.query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+  .then((result) => {
+    if (result.rows[0]) { return result.rows[0] }
+    else { return Promise.reject({ status:404, msg: 'article not found' }) }
+  }
+)}
+
+exports.fetchArticles = () => {
+  
+}
